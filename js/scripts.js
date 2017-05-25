961b1b7858bb097efd1a100a84b4b7a1429b62ae
bjames10 <bryanjames127@eagles.ewu.edu>
2017-05-25 15:17:20 -0700

    tryin to add load

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-// Scripts
-
-// Lightgallery
 /*jslint browser: true*/
 /*global $, jQuery*/
+// Scripts
+$(window).load(function () {
+    'use strict';
+    $('.loader').fadeOut();
+});
+// Lightgallery
+
 $('figure').lightGallery({
     selector: 'a',
     mode: 'lg-slide'
